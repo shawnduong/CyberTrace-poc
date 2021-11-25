b3ef7022cc864c2804f2e47c65d24f1e0b4c79e3
Shawn Duong <shawnduong@protonmail.com>
2021-11-25 12:00:44 -0800

    Added subtitle typewriting.

--- app/static/js/typewriter.js
+++ app/static/js/typewriter.js
@@ -1,13 +1,24 @@
+/* Sleep for ms many milliseconds. */
 function sleep(ms)
 {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function typewriter(object, text)
+/* Typewriter function taking an object to write to, text to write, and speed
+   (latency) in ms between letters written. Writes one character at a time. */
+async function typewriter(object, text, speed)
 {
 	for (let i = 0; i <= text.length; i++)
 	{
-		await sleep(100);
+		await sleep(speed);
 		object.text(text.slice(0, i));
 	}
+}
+
+/* Same as typewriter, but wait a delay in ms before writing. This is akin to
+   a hibernation period before text begins to write. */
+async function typewriter_d(object, text, speed, delay)
+{
+	await sleep(delay);
+	typewriter(object, text, speed);
 }
